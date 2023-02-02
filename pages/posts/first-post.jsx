9ce43564b7ce9a13650a460a641157c8d07ff916
Braien Machado de Paula <braienmp@outlook.com>
2023-02-02 15:10:46 -0300

    feat: assets, metadata and css - add css - add profile image - create Layout component - create _app component

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -2,9 +2,11 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
 
+import Layout from '../../components/layout';
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -16,9 +18,6 @@ export default function FirstPost() {
         }}
       />
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
+    </Layout>
   );
 }
