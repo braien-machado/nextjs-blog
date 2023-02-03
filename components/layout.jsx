0046cb654d3fd8feeff23f8fc3d20771934b84cd
Braien Machado de Paula <braienmp@outlook.com>
2023-02-03 15:12:20 -0300

    feat: create dynamic routes

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,7 +6,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 
 const name = 'Braien Machado';
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'My First Next Website'
 
 export default function Layout({ children, home }) {
   return (
